104e472f13b5b4e1ad353b5affd374447bfe33c1
Lê Thanh Tùng <lethanhtung@tungs-macbook-pro.local>
2016-03-16 20:25:56 +0700

    Initial Commit

---
+++ pokedex-by-TC/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pokedex-by-TC
+//
+//  Created by Lê Thanh Tùng on 3/16/16.
+//  Copyright © 2016 Lê Thanh Tùng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
